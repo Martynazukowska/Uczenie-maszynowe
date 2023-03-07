9ff9afac3209d3d24ff1a1c2ae5a029698ccfb30
Martyna Zukowska <martynazukowska7@interia.pl>
2023-03-07 01:42:17 +0100

    Poczatek

---
+++ klasy/kwiat.py
@@ -0,0 +1,29 @@
+
+class kwiat:
+    def __init__(self,sepal_length,sepal_width,petal_length,petal_width,klasa):
+        self.sepal_length=sepal_length
+        self.sepal_width=sepal_width
+        self.petal_length=petal_length
+        self.petal_width=petal_width
+        self.klasa=klasa
+
+    def get_sepal_length(self):
+        return self.sepal_length
+
+    def get_sepal_length(self):
+        return self.sepal_length
+
+    def get_sepal_length(self):
+        return self.sepal_length
+
+    def get_sepal_length(self):
+        return self.sepal_length
+
+    def get_sepal_length(self):
+        return self.sepal_length
+
+    def get_sepal_length(self):
+        return self.sepal_length
+
+    def get_sepal_length(self):
+        return self.sepal_length
---
+++ main.py
@@ -0,0 +1,12 @@
+
+file_irys=open("./IRYS/iris.data","r+")
+
+#print(file_irys.read())
+
+wiersz=file_irys.read().split("\n")
+
+for ilosc in wiersz:
+    
+
+
+print(wiersz[0])
